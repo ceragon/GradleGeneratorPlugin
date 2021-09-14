9a55f6e8d505c2458886f4b2ad22bfe493f49108
ceragon <wangxkaixwu@163.com>
2021-09-14 19:38:09 +0800

    调整一版本目录结构，以便于发布到gradle仓库

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GradleGeneratorPlugin
 作为gradle插件，提供protobuf生成及衍生功能
+
+## protoBuf 生成
--- plugin/src/main/java/com/ceragon/protobuf/protoc/MsgCodeBuild.java
+++ plugin/src/main/java/com/ceragon/protobuf/protoc/MsgCodeBuild.java
@@ -14,7 +14,6 @@ import lombok.Value;
 import org.gradle.api.logging.Logger;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
