cef0349ab9e44f238bd7210053f16fb4a8bac72a
wangkaiwu <wangxkaixwu@163.com>
2021-09-22 22:23:26 +0800

    升级velocity版本，消除无意义空白

--- plugin/src/main/java/io/github/ceragon/util/CodeGenTool.java
+++ plugin/src/main/java/io/github/ceragon/util/CodeGenTool.java
@@ -17,15 +17,16 @@ public class CodeGenTool {
     private CodeGenTool() {
     }
 
-    public static void createCodeByPath(String sourcePath, String sourceName, String desPath,boolean overwrite, Map<String, Object> content) throws IOException {
+    public static void createCodeByPath(String sourcePath, String sourceName, String desPath, boolean overwrite, Map<String, Object> content) throws IOException {
         VelocityEngine ve = new VelocityEngine();
         ve.setProperty(VelocityEngine.INPUT_ENCODING, "UTF-8");
         ve.setProperty(VelocityEngine.FILE_RESOURCE_LOADER_PATH, sourcePath);
+        ve.setProperty("globbing.resource.loader.class", "StructuredGlobbingResourceLoader");
         ve.init();
-        createCode(ve, sourceName, desPath,overwrite, content);
+        createCode(ve, sourceName, desPath, overwrite, content);
     }
 
-    public static void createCodeByClasspath(String sourceName, String desPath,boolean overwrite, Map<String, Object> content) throws IOException {
+    public static void createCodeByClasspath(String sourceName, String desPath, boolean overwrite, Map<String, Object> content) throws IOException {
         VelocityEngine ve = new VelocityEngine();
         ve.setProperty(VelocityEngine.INPUT_ENCODING, "UTF-8");
         ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
